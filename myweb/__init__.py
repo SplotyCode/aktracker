5bb826bc85538786347060e3913a45bea510e759
David <davidscandurra@gmail.com>
2022-06-09 11:04:42 +0000

    Experiment with yfinance

--- myweb/__init__.py
+++ myweb/__init__.py
@@ -1,3 +1,9 @@
 import pymysql
 
-pymysql.install_as_MySQLdb()
+#pymysql.install_as_MySQLdb()
+
+import yfinance as yf
+
+# Get the data for the stock AAPL
+data = yf.download('AAPL','2016-01-01','2019-08-01')
+print(data)
